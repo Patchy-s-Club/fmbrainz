0846c7bae7212ffee4d800e0a7c2beb77ffdd0a8
Alan Miles <thesorton@icloud.com>
2024-04-30 09:56:11 -0500

    inital *brainz support

--- src/WebServices/ListenBrainz.cs
+++ src/WebServices/ListenBrainz.cs
@@ -1,19 +1,62 @@
-﻿namespace fmbrainz
+﻿using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace fmbrainz.WebServices
 {
     internal class ListenBrainz
     {
-        private static string baseUrl = "https://api.listenbrainz.org";
+        private static string lbUrl = "https://api.listenbrainz.org";
+        private static string mbUrl = "https://musicbrainz.org/ws/2";
+        private static readonly HttpClient client = new()
+        {
+            DefaultRequestHeaders =
+            {
+                UserAgent = { ProductInfoHeaderValue.Parse("fmbrainz/1.0") }
+            }
+        };
 
+        private static async Task<T> LookupMetadata<T>(string mediaType, string name, string artistName, string incs)
+        {
+            string url ="";
+            switch (mediaType)
+            {
+                case "track":
+                    url =
+                        $"{lbUrl}/1/metadata/lookup/?recording_name={Uri.EscapeDataString(name)}&artist_name={Uri.EscapeDataString(artistName)}";
+                    break;
+                case "album":
+                    url = 
+                        $"{mbUrl}/release/?query=release:{Uri.EscapeDataString(name)}%20AND%20artist:{Uri.EscapeDataString(artistName)}&fmt=json";
+                    break;
+                default:
+                    throw new Exception("Invalid media type.");
+            }
+
+            Console.WriteLine(url);
+            if (!string.IsNullOrEmpty(incs))
+            {
+                url += $"&metadata=true&incs={Uri.EscapeDataString(incs)}";
+            }
+
+            var response = await client.GetAsync(url);
+            //response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<T>(responseContent);
+
+            return data;
+        }
+        public static async Task<string> GetMusicBrainzId(string entityType, string name, string artist)
+        {
+            var response = await LookupMetadata<dynamic>(entityType, name, artist, null);
+            return response.releases[0].id;
+        }
         public static async Task<dynamic> GetListens(string username, string token,
             long? minTs = null, long? maxTs = null, int? count = null)
         {
-            var url = $"{baseUrl}/1/user/{username}/listens";
-            var parameters = new Dictionary<string, string>();
-            if (minTs.HasValue) parameters.Add("min_ts", minTs.Value.ToString());
-            if (maxTs.HasValue) parameters.Add("max_ts", maxTs.Value.ToString());
-            if (count.HasValue) parameters.Add("count", count.Value.ToString());
+            var url = $"{lbUrl}/1/user/{username}/listens";
 
-            var jsonResponse = await HttpService.GetResponse<dynamic>(token, url, parameters);
+            var jsonResponse = await HttpService.GetResponse<dynamic>(token, url);
 
             return jsonResponse["payload"]["listens"];
         }
