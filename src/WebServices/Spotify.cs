13d94bd86c5f68c758306747ef6ccbf6c06b4780
Skylar Miles <thesorton@gmail.com>
2024-04-30 12:46:28 -0500

    add Spotify service

---
+++ src/WebServices/Spotify.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+
+namespace fmbrainz.WebServices;
+
+public class Spotify
+{
+    private static readonly string? token =
+        "x";
+    private static readonly string spotifyUrl = "https://api.spotify.com/v1/search";
+
+    private readonly HttpClient client = new()
+    {
+        DefaultRequestHeaders =
+        {
+            UserAgent = { ProductInfoHeaderValue.Parse("fmbrainz/1.0") }
+        }
+    };
+    public Spotify()
+    {
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    }
+
+    public static async Task<dynamic> GetArtistImage(string artist)
+    {
+        var parameters = new Dictionary<string, string>
+        {
+            { "q", "artist:" + artist },
+            { "type", "artist" },
+            { "market", "US" },
+            { "limit", "1" }
+        };
+        var response = await HttpService.GetResponse<dynamic>(token, spotifyUrl, parameters);
+        return response.artists.items[0].images[0];
+    }
+}
