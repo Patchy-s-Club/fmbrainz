c4e408a40c84bd64dcc2685259bce1d9329231dc
Alan Miles <thesorton@icloud.com>
2024-04-30 09:59:19 -0500

    add a listenbrainz command

---
+++ src/Commands/ListenBrainz.cs
@@ -0,0 +1,34 @@
+using Discord;
+using Discord.WebSocket;
+using fmbrainz.WebServices;
+
+namespace fmbrainz.Commands;
+
+public class lb : Command
+{
+    public override string Name => "lb";
+    public override string Description => "Parent lb command.";
+
+    public override List<SlashCommandOptionBuilder> Options =>
+    [
+        new SlashCommandOptionBuilder()
+            .WithName("getuserlistens")
+            .WithDescription("Get user listens.")
+            .WithType(ApplicationCommandOptionType.SubCommand)
+            .AddOption("username", ApplicationCommandOptionType.String, "The username of the user.", true)
+            .WithRequired(false)
+    ];
+
+    public override async Task ExecuteAsync(SocketSlashCommand command)
+    {
+        var subCommand = command.Data.Options.First().Name;
+        switch (subCommand)
+        {
+            case "getuserlistens":
+                var username = command.Data.Options.First().Options.First().Value.ToString();
+                var user = await ListenBrainz.GetListens(username, null);
+                await command.RespondAsync(embed: EmbedResponse.GetLbListensEmbed(user));
+                break;
+        }
+    }
+}
